e40904635da8e862b3c386885d00f000c0f1498d
Efex16 <efeefosa84@gmail.com>
2023-09-06 02:38:35 +0100

    first commit

---
+++ frontend/src/components/TodoListView.js
@@ -0,0 +1,14 @@
+// @bekbrace
+// EFEX_WEB - SEptember 2023
+
+import TodoItem from './Todo'
+
+export default function TodoView(props) {
+    return (
+        <div>
+            <ul>
+                {props.todoList.map(todo => <TodoItem todo={todo} />)}
+            </ul>
+        </div>
+    )
+}
